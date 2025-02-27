86e45c5182401902aab87d39bce9ebf307520d3d
Suhaib Khater <sohib.khater1@hotmail.com>
2025-02-27 21:56:45 +0300

    change response

--- helper/helper.ts
+++ helper/helper.ts
@@ -1,22 +1,2 @@
 // MKDN Helper Response
-export const simulatedResponse:string = `# Interview Assessment
-
-## Features
-
-### Prisma Schema
-
-Create a schema using Prisma where the following is stored:
-- User chat history
-- User starred prompt/response
-
-### UI Enhancements
-
-- Separate the responses in the UI as:
-  - Left: AI
-  - Right: User
-- Store the chat in the database
-
-### Starred Page
-
-Add a page called '/starred' where the user can the prompts and responses they have starred, and the LLM used to generate the response.
-`;
+export const simulatedResponse:string = `# This is a simulated response`;
